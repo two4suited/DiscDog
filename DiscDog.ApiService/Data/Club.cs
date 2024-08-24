a0afa1d593e208ff5afdf19be56369cf1c978460
Brian Sheridan <brian.sheridan@gmail.com>
2024-08-24 16:24:20 -0700

    chore: Remove unused OpenApi files and update gitignore

--- DiscDog.ApiService/Data/Club.cs
+++
@@ -1,8 +0,0 @@
-namespace DiscDog.ApiService;
-
-public class Club
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<Team> Teams { get; set; } = new List<Team>();
-}
