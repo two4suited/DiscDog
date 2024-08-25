1930ba3ea4c9f9fe1e2ba516e4cc5c0a7d392dac
Brian Sheridan <brian.sheridan@gmail.com>
2024-08-24 17:22:21 -0700

    chore: Update Postgres configuration in Program.cs

--- src/DiscDog.AppHost/Program.cs
+++ src/DiscDog.AppHost/Program.cs
@@ -1,12 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
-var postgresdb = postgres.AddDatabase("postgresdb");
+var postgres = builder.AddPostgres("postgres").WithPgAdmin().AddDatabase("postgresdb");
+
 
 var apiService = builder.AddProject<Projects.DiscDog_ApiService>("apiservice");
 
 builder.AddProject<Projects.DiscDog_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(apiService).WithReference(postgresdb);
+    .WithReference(apiService).WithReference(postgres);
 
 builder.Build().Run();
